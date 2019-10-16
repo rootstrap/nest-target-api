4f8e9e7280371c099d64e76ac4d73998b75685f7
Alexis Zucco <alexis@rootstrap.com>
2019-10-16 18:27:43 -0300

    Config dotenv to manage env variables (#3)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [ConfigModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
